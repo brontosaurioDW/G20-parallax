dfa8fe29a0b126e00193538e329106b9ed410aa2
fespinoX <florencia.espino@gmail.com>
2018-10-09 01:08:23 -0300

    Mapa listo :D

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
   $(document).ready(function(){
     $('.parallax').parallax();
+
+	$('.tooltip').tooltipster({
+
+		animation: 'fade',
+		delay: 200,
+		theme: ['tooltipster-shadow', 'tooltipster-shadow-customized'],
+		trigger: 'click',
+		contentCloning: true
+	    
+	});
+
   });
+
+
+
+
