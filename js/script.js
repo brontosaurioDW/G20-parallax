81e8df1f77716bf1ed0b36972f6919e70985857f
fespinoX <florencia.espino@gmail.com>
2018-10-12 02:10:58 -0300

    correcciones

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
   $(document).ready(function(){
+
+  	/* starts parallax */
+
     $('.parallax').parallax();
+
+
+    /* starts tooltipster */
 
 	$('.tooltip').tooltipster({
 
@@ -11,6 +17,92 @@
 	    
 	});
 
+	
+	/* activates tooltipster second button in desktop */
+
+	$('#colonbtn').click(function(){
+	    $("#colonttbtn").click();
+	})
+
+	$('#obeliscobtn').click(function(){
+	    $("#obeliscottbtn").click();
+	})
+
+	$('#casarosadabtn').click(function(){
+	    $("#casarosadattbtn").click();
+	})
+
+	$('#cckbtn').click(function(){
+	    $("#cckttbtn").click();
+	})
+
+	$('#costasalguerobtn').click(function(){
+	    $("#costasalguerottbtn").click();
+	})
+
+	$('#palaciosanmartinbtn').click(function(){
+	    $("#palaciosanmartinttbtn").click();
+	})
+
+	$('#casadegobiernobtn').click(function(){
+	    $("#casadegobiernottbtn").click();
+	})
+
+	$('#cecbtn').click(function(){
+	    $("#cecttbtn").click();
+	})
+
+	$('#caminitobtn').click(function(){
+	    $("#caminitottbtn").click();
+	})
+
+	$('#villaolimpicabtn').click(function(){
+	    $("#villaolimpicattbtn").click();
+	})
+
+/*
+
+	$('#colonbtnmob').click(function(){
+	    $("#colonttbtnmob").click();
+	})
+
+	$('#obeliscobtnmob').click(function(){
+	    $("#obeliscottbtnmob").click();
+	})
+
+	$('#casarosadabtnmob').click(function(){
+	    $("#casarosadattbtnmob").click();
+	})
+
+	$('#cckbtnmob').click(function(){
+	    $("#cckttbtnmob").click();
+	})
+
+	$('#costasalguerobtnmob').click(function(){
+	    $("#costasalguerottbtnmob").click();
+	})
+
+	$('#palaciosanmartinbtnmob').click(function(){
+	    $("#palaciosanmartinttbtnmob").click();
+	})
+
+	$('#casadegobiernobtnmob').click(function(){
+	    $("#casadegobiernottbtnmob").click();
+	})
+
+	$('#cecbtnmob').click(function(){
+	    $("#cecttbtnmob").click();
+	})
+
+	$('#caminitobtnmob').click(function(){
+	    $("#caminitottbtnmob").click();
+	})
+
+	$('#villaolimpicabtnmob').click(function(){
+	    $("#villaolimpicattbtnmob").click();
+	})
+
+*/
   });
 
 
